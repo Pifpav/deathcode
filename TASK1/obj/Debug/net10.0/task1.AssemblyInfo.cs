ea2e0fd065a3e5b735f105a8a7d0a88d05a571ed
Alex <pifpav.death@yandex.ru>
2026-04-30 13:48:27 +0300

    add new function

--- TASK1/obj/Debug/net10.0/task1.AssemblyInfo.cs
+++ TASK1/obj/Debug/net10.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc126bf5ff12357df08ecb2cae38199f5b3a5f01")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
