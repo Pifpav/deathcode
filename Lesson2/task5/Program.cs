7b344bdcd0c53dd0c1a63e568877b63e006f63f0
Alex <pifpav.death@yandex.ru>
2026-05-04 15:52:39 +0300

    Add new task:task5

---
+++ Lesson2/task5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int size = 10;
+        int[] arr_int = new int[size];
+        int i = 0;
+        
+        while (i < size)
+        {
+            Console.Write($"Введите элемент [{i + 1}/{size}]: ");
+            string input = Console.ReadLine();
+            
+            // Проверка корректности ввода
+            if (int.TryParse(input, out int number))
+            {
+                arr_int[i] = number;
+                i++;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Введите целое число.");
+            }
+        }
+        
+        // Вывод результата
+        Console.WriteLine("\nГотовый массив:");
+        for (int j = 0; j < size; j++)
+        {
+            Console.WriteLine($"arr_int[{j}] = {arr_int[j]}");
+        }
+        
+        Console.WriteLine("\nНажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+}
