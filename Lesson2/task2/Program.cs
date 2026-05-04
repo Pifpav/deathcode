f2ab25f87af195591f6ab031c7e498db76516340
Alex <pifpav.death@yandex.ru>
2026-05-04 12:30:20 +0300

    add function1

--- Lesson2/task2/Program.cs
+++ Lesson2/task2/Program.cs
@@ -1,10 +1,10 @@
 ﻿double CalculateFormula(int a, int b, int c, int d)
 {
-   double int numenator =  a * b;
-   int denamenator = c + d;
+   double numenator = a * b;
+   int denomenator = c + d;
    double result = numenator / denomenator;
    return result;
 }
 
-double result = CalculateFormula(1,2,3,4);
+double result = CalculateFormula(1, 2, 3, 4);
 Console.WriteLine(result);
