9620eda8645f707e4a33f3569d9c4a5a22571034
Alex <pifpav.death@yandex.ru>
2026-05-04 16:46:02 +0300

    add array1

--- lesson3/task1/Program.cs
+++ lesson3/task1/Program.cs
@@ -1,19 +1,21 @@
-﻿int[,] matrix = new int[4, 5];
+﻿int[,] matrix = new int[3, 5];
 Random rnd = new Random();
 
 for (int i = 0; i < matrix.GetLength(0); i++)
 {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
+
         matrix[i, j] = rnd.Next(1, 11);
     }
 }
-
 
 for (int i = 0; i < matrix.GetLength(0); i++)
 {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        Console.WriteLine($"{matrix[i, j]}");
+
+        Console.Write($"{matrix[i, j]} ");
     }
+    Console.WriteLine();
 }
